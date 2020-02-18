644f9c53117229e57b5307f9ac73cda0c00753ed
Tiffany Manthey <tcmanthey@coastal.edu>
2020-02-18 18:45:20 -0500

    hotels

---
+++ database/migrations/2020_02_18_233458_create_rooms_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('hotel_id');
+            $table->string('type');
+            $table->string('description');
+            $table->decimal('price', 10, 2);
+            $table->string('image');
+            $table->timestamps();
+
+            $table->foreign('hotel_id')->references('id')->on('hotels');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
