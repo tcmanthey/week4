3a3bf0541b833bd74c17a9487f007f9a6b0e67f5
Tiffany Manthey <tcmanthey@coastal.edu>
2020-02-18 19:13:06 -0500

    room

---
+++ app/Models/Room.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Room extends Model
+{
+    protected $fillable = [
+        'hotel_id',
+        'type',
+        'description',
+        'price',
+        'image',
+    ];
+
+    public function hotel() {
+        return $this->belongsTo('App\Models\Hotel');
+    }
+}
