93d08ad2d5a5bbe2794c4a1dc3ee96e0512db0d6
Tiffany Manthey <tcmanthey@coastal.edu>
2020-02-22 18:16:28 -0500

    Loaded seed data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            UsersTableSeeder::class,
+            HotelsTableSeeder::class,
+            RoomsTableSeeder::class,
+            ReservationsTableSeeder::class,
+        ]);
     }
 }
