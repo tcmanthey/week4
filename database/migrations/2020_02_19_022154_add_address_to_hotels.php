d3d7e8423eede64cb565705625bbe6bfc61f33ba
Tiffany Manthey <tcmanthey@coastal.edu>
2020-02-22 17:40:20 -0500

    Created Seeder

---
+++ database/migrations/2020_02_19_022154_add_address_to_hotels.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAddressToHotels extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('hotels', function (Blueprint $table) {
+            $table->renameColumn('name', 'hotels');
+            $table->string('address_2')->nullable();
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('hotels', function (Blueprint $table) {
+            $table->renameColumn(‘location’, ‘hotels’);
+        });
+    }
+}
