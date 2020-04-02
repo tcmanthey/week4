f37b906921ebd43b7e52f23fd6a76f144b234768
tcmanthey <59845936+tcmanthey@users.noreply.github.com>
2020-04-02 11:04:28 -0400

    Added create hotel form

---
+++ resources/views/hotelform.blade.php
@@ -0,0 +1,12 @@
+<form action="/hotels/store.php" method="post">
+
+    <input type="text" id="name" name="Name:"><br><br>
+    <input type="text" id="address1" name="Address1:"><br><br>
+    <input type="text" id="address2" name="Address2:"><br><br>
+    <input type="text" id="city" name="City:"><br><br>
+    <input type="text" id="state" name="State:"><br><br>
+    <input type="text" id="zip" name="Zip:"><br><br>
+    <input type="text" id="description" name="Description:"><br><br>
+    <input type="image" id="image" name="Image:" alt="image"><br><br>
+    <input type="submit" value="Submit">
+</form>
