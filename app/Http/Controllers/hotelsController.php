b4a3b55c1e979734e1143a0e4c7cb905792ae8de
tcmanthey <59845936+tcmanthey@users.noreply.github.com>
2020-04-02 11:21:26 -0400

    updated

--- app/Http/Controllers/hotelsController.php
+++ app/Http/Controllers/hotelsController.php
@@ -1,3 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Hotel;
+
+class HotelController extends Controller
+{
+    public function index(){
+        $hotels = Hotel::all();
+        return view('hotels')->with('hotels', $hotels);
+    }
+}
 public function create(Request $request)
     {
         $name = $request->input('hotelfomr');
